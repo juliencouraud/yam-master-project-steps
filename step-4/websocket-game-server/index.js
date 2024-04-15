40cc6c1c95840bcec20ea85edfcd218daf8fa910
juliencouraud <couraud.julien@gmail.com>
2024-04-15 21:57:49 +0200

    add step 4 - Global Layout

---
+++ step-4/websocket-game-server/index.js
@@ -0,0 +1,80 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+var uniqid = require('uniqid');
+const GameService = require('./services/game.service');
+
+// ---------------------------------------------------
+// -------- CONSTANTS AND GLOBAL VARIABLES -----------
+// ---------------------------------------------------
+let games = [];
+let queue = [];
+
+// ---------------------------------
+// -------- GAME METHODS -----------
+// ---------------------------------
+
+const newPlayerInQueue = (socket) => {
+
+  queue.push(socket);
+
+  // Gestion de la 'queue'
+  if (queue.length >= 2) {
+    const player1Socket = queue.shift();
+    const player2Socket = queue.shift();
+    createGame(player1Socket, player2Socket);
+  }
+  else {
+    socket.emit('queue.added', GameService.send.forPlayer.viewQueueState());
+  }
+};
+
+const createGame = (player1Socket, player2Socket) => {
+
+  // Initialisation d'un objet (game) avec un premier niveau de structure:
+  // - gameState : { .. objet évolutif de la partie .. }
+  // - idGame : au cas où
+  // - player1Socket: instance de la socket du "joueur:1"
+  // - player2Socket: instance de la socket du "joueur:2"
+  const newGame = GameService.init.gameState();
+  newGame['idGame'] = uniqid();
+  newGame['player1Socket'] = player1Socket;
+  newGame['player2Socket'] = player2Socket;
+
+  // On ajoute la partie au tableau de parties
+  games.push(newGame);
+
+  // On cherche l'index pour accéder à l'objet de partie (à partir de maintenant nous manipulerons l'objet par son index dans le tableau
+  const gameIndex = GameService.utils.findGameIndexById(games, newGame.idGame);
+
+  // On 'emit' aux deux joueurs un objet customisé pour leur vue.
+  games[gameIndex].player1Socket.emit('game.start', GameService.send.forPlayer.viewGameState('player:1', games[gameIndex]));
+  games[gameIndex].player2Socket.emit('game.start', GameService.send.forPlayer.viewGameState('player:2', games[gameIndex]));
+};
+
+// ---------------------------------------
+// -------- SOCKETS MANAGEMENT -----------
+// ---------------------------------------
+
+io.on('connection', socket => {
+  console.log(`[${socket.id}] socket connected`);
+
+  socket.on('queue.join', () => {
+    console.log(`[${socket.id}] new player in queue `)
+    newPlayerInQueue(socket);
+  });
+
+  socket.on('disconnect', reason => {
+    console.log(`[${socket.id}] socket disconnected - ${reason}`);
+  });
+});
+
+// -----------------------------------
+// -------- SERVER METHODS -----------
+// -----------------------------------
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
