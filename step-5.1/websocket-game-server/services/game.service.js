a195717aa8eebe94f9cc2fa204a00500bbfc3b59
juliencouraud <couraud.julien@gmail.com>
2024-04-15 23:25:41 +0200

    add step 5.1 - Timer

---
+++ step-5.1/websocket-game-server/services/game.service.js
@@ -0,0 +1,75 @@
+// Durée d'un tour en secondes
+const TURN_DURATION = 30;
+ 
+const GAME_INIT = {
+    gameState: {
+        currentTurn: 'player:1',
+        timer: TURN_DURATION,
+        player1Score: 0,
+        player2Score: 0,
+        grid: [],
+        choices: {},
+        deck: {}
+    }
+}
+
+const GameService = {
+
+    init: {
+        gameState: () => {
+            return GAME_INIT;
+        },
+    },
+    
+    send: {
+        forPlayer: {
+            viewGameState: (playerKey, game) => {
+                return {
+                    inQueue: false,
+                    inGame: true,
+                    idPlayer: 
+                        (playerKey === 'player:1')
+                            ? game.player1Socket.id
+                            : game.player2Socket.id,
+                    idOpponent:
+                        (playerKey === 'player:1')
+                            ? game.player2Socket.id
+                            : game.player1Socket.id
+                };
+            },
+            
+            viewQueueState: () => {
+                return {
+                    inQueue: true,
+                    inGame: false,
+                };
+            },
+
+            gameTimer: (playerKey, gameState) => {
+                const playerTimer = gameState.currentTurn === playerKey ? gameState.timer : 0;
+                const opponentTimer = gameState.currentTurn === playerKey ? 0 : gameState.timer;
+                return { playerTimer: playerTimer, opponentTimer: opponentTimer };
+            },
+        }
+    },
+
+    timer: {
+        getTurnDuration: () => {
+            return TURN_DURATION;
+        }
+    },
+
+    utils: {
+        // Return game index in global games array by id
+        findGameIndexById: (games, idGame) => {
+            for (let i = 0; i < games.length; i++) {
+                if (games[i].idGame === idGame) {
+                    return i; // Retourne l'index du jeu si le socket est trouvé
+                }
+            }
+            return -1;
+        },
+    }
+}
+
+module.exports = GameService;
