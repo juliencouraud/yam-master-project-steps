ecadb51e21dab621de7f3ce080ae1ab654b92a2c
juliencouraud <couraud.julien@gmail.com>
2024-04-16 20:35:59 +0200

    fix step-5

---
+++ step-5-2/app/components/board/board.component.js
@@ -0,0 +1,221 @@
+import React, { useEffect, useState, useContext } from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import { SocketContext } from '../../contexts/socket.context';
+import PlayerDeck from "./decks/player-deck.component";
+import OpponentDeck from "./decks/opponent-deck.component";
+
+const OpponentInfos = () => {
+  return (
+    <View style={styles.opponentInfosContainer}>
+      <Text>Opponent infos</Text>
+    </View>
+  );
+};
+
+const OpponentTimer = () => {
+  const socket = useContext(SocketContext);
+  const [opponentTimer, setOpponentTimer] = useState(0);
+
+  useEffect(() => {
+
+    socket.on("game.timer", (data) => {
+      // console.log("'game.timer' WS triggered in OpponentTimer component: [", data['opponentTimer'], "]");
+      setOpponentTimer(data['opponentTimer'])
+    });
+
+  }, []);
+  return (
+    <View style={styles.opponentTimerContainer}>
+      <Text>Timer: {opponentTimer}</Text>
+    </View>
+  );
+};
+
+const OpponentScore = () => {
+  return (
+    <View style={styles.opponentScoreContainer}>
+      <Text>Score: </Text>
+    </View>
+  );
+};
+
+const Grid = () => {
+
+  return (
+    <View style={styles.gridContainer}>
+      <Text>Grid</Text>
+    </View>
+  );
+};
+
+const Choices = () => {
+
+  return (
+    <View style={styles.choicesContainer}>
+      <Text>Choices</Text>
+    </View>
+  );
+};
+
+const PlayerInfos = () => {
+  return (
+    <View style={styles.playerInfosContainer}>
+      <Text>Player Infos</Text>
+    </View>
+  );
+};
+
+const PlayerTimer = () => {
+  const socket = useContext(SocketContext);
+  const [playerTimer, setPlayerTimer] = useState(0);
+
+  useEffect(() => {
+    socket.on("game.timer", (data) => {
+      setPlayerTimer(data['playerTimer'])
+    });
+  }, []);
+
+  return (
+    <View style={styles.playerTimerContainer}>
+      <Text>Timer: {playerTimer}</Text>
+    </View>
+  );
+};
+
+const PlayerScore = () => {
+
+  return (
+    <View style={styles.playerScoreContainer}>
+      <Text>PlayerScore</Text>
+    </View>
+  );
+};
+
+const Board = ({ gameViewState }) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.row, { height: '5%' }]}>
+        <OpponentInfos />
+        <View style={styles.opponentTimerScoreContainer}>
+          <OpponentTimer />
+          <OpponentScore />
+        </View>
+      </View>
+      <View style={[styles.row, { height: '25%' }]}>
+        <OpponentDeck />
+      </View>
+      <View style={[styles.row, { height: '40%' }]}>
+        <Grid />
+        <Choices />
+      </View>
+      <View style={[styles.row, { height: '25%' }]}>
+        <PlayerDeck />
+      </View>
+      <View style={[styles.row, { height: '5%' }]}>
+        <PlayerInfos />
+        <View style={styles.playerTimerScoreContainer}>
+          <PlayerTimer />
+          <PlayerScore />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+    width: '100%',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+  },
+  opponentInfosContainer: {
+    flex: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderColor: 'black',
+    backgroundColor: "lightgrey"
+  },
+  opponentTimerScoreContainer: {
+    flex: 3,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
+  },
+  opponentTimerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  opponentScoreContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  deckOpponentContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "black"
+  },
+  gridContainer: {
+    flex: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderColor: 'black',
+  },
+  choicesContainer: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
+  },
+  deckPlayerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+  },
+  playerInfosContainer: {
+    flex: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderColor: 'black',
+    backgroundColor: "lightgrey"
+  },
+  playerTimerScoreContainer: {
+    flex: 3,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
+  },
+  playerTimerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
+  },
+  playerScoreContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "lightgrey"
+  },
+});
+
+export default Board;
---
+++ step-5-2/websocket-game-server/services/game.service.js
@@ -0,0 +1,159 @@
+// Durée d'un tour en secondes
+const TURN_DURATION = 30;
+
+const DECK_INIT = {
+    dices: [
+        { id: 1, value: '', locked: true },
+        { id: 2, value: '', locked: true },
+        { id: 3, value: '', locked: true },
+        { id: 4, value: '', locked: true },
+        { id: 5, value: '', locked: true },
+    ],
+    rollsCounter: 1,
+    rollsMaximum: 3
+};
+
+const GAME_INIT = {
+    gameState: {
+        currentTurn: 'player:1',
+        timer: null,
+        player1Score: 0,
+        player2Score: 0,
+        grid: [],
+        choices: {},
+        deck: {}
+    }
+}
+
+const GameService = {
+
+    init: {
+        gameState: () => {
+            const game = { ...GAME_INIT };
+            game['gameState']['timer'] = TURN_DURATION;
+            game['gameState']['deck'] = { ...DECK_INIT };
+            return game;
+        },
+
+        deck: () => {
+            return { ...DECK_INIT };
+        },
+    },
+
+    send: {
+        forPlayer: {
+            viewGameState: (playerKey, game) => {
+                return {
+                    inQueue: false,
+                    inGame: true,
+                    idPlayer:
+                        (playerKey === 'player:1')
+                            ? game.player1Socket.id
+                            : game.player2Socket.id,
+                    idOpponent:
+                        (playerKey === 'player:1')
+                            ? game.player2Socket.id
+                            : game.player1Socket.id
+                };
+            },
+
+            viewQueueState: () => {
+                return {
+                    inQueue: true,
+                    inGame: false,
+                };
+            },
+
+            gameTimer: (playerKey, gameState) => {
+                const playerTimer = gameState.currentTurn === playerKey ? gameState.timer : 0;
+                const opponentTimer = gameState.currentTurn === playerKey ? 0 : gameState.timer;
+                return { playerTimer: playerTimer, opponentTimer: opponentTimer };
+            },
+
+            deckViewState: (playerKey, gameState) => {
+                const deckViewState = {
+                    displayPlayerDeck: gameState.currentTurn === playerKey,
+                    displayOpponentDeck: gameState.currentTurn !== playerKey,
+                    displayRollButton: gameState.deck.rollsCounter <= gameState.deck.rollsMaximum,
+                    rollsCounter: gameState.deck.rollsCounter,
+                    rollsMaximum: gameState.deck.rollsMaximum,
+                    dices: gameState.deck.dices
+                };
+                return deckViewState;
+            }
+        }
+    },
+
+    timer: {  
+        getTurnDuration: () => {
+            return TURN_DURATION;
+        }
+    },
+
+    dices: {
+        roll: (dicesToRoll) => {
+            const rolledDices = dicesToRoll.map(dice => {
+                if (dice.value === "") {
+                    // Si la valeur du dé est vide, alors on le lance en mettant le flag locked à false
+                    const newValue = String(Math.floor(Math.random() * 6) + 1); // Convertir la valeur en chaîne de caractères
+                    return {
+                        id: dice.id,
+                        value: newValue,
+                        locked: false
+                    };
+                } else if (!dice.locked) {
+                    // Si le dé n'est pas verrouillé et possède déjà une valeur, alors on le relance
+                    const newValue = String(Math.floor(Math.random() * 6) + 1);
+                    return {
+                        ...dice,
+                        value: newValue
+                    };
+                } else {
+                    // Si le dé est verrouillé ou a déjà une valeur mais le flag locked est true, on le laisse tel quel
+                    return dice;
+                }
+            });
+            return rolledDices;
+        },
+
+        lockEveryDice: (dicesToLock) => {
+            const lockedDices = dicesToLock.map(dice => ({
+                ...dice,
+                locked: true // Verrouille chaque dé
+            }));
+            return lockedDices;
+        }
+    },
+
+    utils: {
+        // Return game index in global games array by id
+        findGameIndexById: (games, idGame) => {
+            for (let i = 0; i < games.length; i++) {
+                if (games[i].idGame === idGame) {
+                    return i; // Retourne l'index du jeu si le socket est trouvé
+                }
+            }
+            return -1;
+        },
+
+        findGameIndexBySocketId: (games, socketId) => {
+            for (let i = 0; i < games.length; i++) {
+                if (games[i].player1Socket.id === socketId || games[i].player2Socket.id === socketId) {
+                    return i; // Retourne l'index du jeu si le socket est trouvé
+                }
+            }
+            return -1;
+        },
+
+        findDiceIndexByDiceId: (dices, idDice) => {
+            for (let i = 0; i < dices.length; i++) {
+                if (dices[i].id === idDice) {
+                    return i; // Retourne l'index du jeu si le socket est trouvé
+                }
+            }
+            return -1;
+        }
+    }
+}
+
+module.exports = GameService;
