18d44f13e8c9d0414fa994316af0ca6859b20782
juliencouraud <couraud.julien@gmail.com>
2024-04-17 20:51:40 +0200

    add step 7

---
+++ step-7/app/screens/online-game.screen.js
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { StyleSheet, View, Button, Text } from "react-native";
+import { SocketContext } from '../contexts/socket.context';
+import OnlineGameController from "../controllers/online-game.controller";
+
+export default function OnlineGameScreen({ navigation }) {
+
+    const socket = useContext(SocketContext);
+
+    return (
+        <View style={styles.container}>
+
+            {!socket && (
+                <>
+                    <Text style={styles.paragraph}>
+                        No connection with server...
+                    </Text>
+                    <Text style={styles.footnote}>
+                        Restart the app and wait for the server to be back again.
+                    </Text>
+                    <Button
+                        title="Revenir au menu"
+                        onPress={() => navigation.navigate('HomeScreen')}
+                    />
+                </>
+            )}
+
+            {socket && (
+                <>
+                    <OnlineGameController />
+                </>
+            )}
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    }
+});
